d60fedf537a98eac638864b18b915d1d37d36833
Chea Ritheavatey <2023447chea@aupp.edu.kh>
2026-03-07 16:17:08 +0100

    Update Province.java

--- localmarket/src/main/java/com/example/localmarket/entity/Province.java
+++ localmarket/src/main/java/com/example/localmarket/entity/Province.java
@@ -1,8 +1,19 @@
 package com.example.localmarket.entity;
 
 public enum Province {
-    TAKEO,
-    KAMPOT,
-    SIEM_REAP,
-    PHNOM_PENH
+    TAKEO("Takeo"),
+    KAMPOT("Kampot"),
+    SIEM_REAP("Siem Reap"),
+    PHNOM_PENH("Phnom Penh");
+
+    private final String name;
+
+    Province(String name) {
+        this.name = name;
+    }
+
+    // show province name
+    public String getName() {
+        return name;
+    }
 }
