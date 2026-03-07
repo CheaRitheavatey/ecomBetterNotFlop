35ed2f9e5636637321f953920070432020dc639b
Chea Ritheavatey <2023447chea@aupp.edu.kh>
2026-03-07 16:15:01 +0100

    Update category entity

--- localmarket/src/main/java/com/example/localmarket/entity/Category.java
+++ localmarket/src/main/java/com/example/localmarket/entity/Category.java
@@ -1,8 +1,23 @@
 package com.example.localmarket.entity;
 
 public enum Category {
-    ALL_CATEGORY,
-    FOOD_AND_BEVERAGE,
-    CLOTHES,
-    GIFTS
+    ALL_CATEGORY("All Categories"),
+    FOOD_AND_BEVERAGE("Food & Beverage"),
+    CLOTHES("Clothes"),
+    GIFTS("Gifts & Crafts"),
+    ElECTRONICS("Electronics"),
+    HOME_AND_LIVING("Home & Living"),
+    HEALTH_AND_BEAUTY("Health & Beauty"),
+    AGRICULTURE("Agriculture");
+
+    private final String displayName;
+
+    Category(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
 }
